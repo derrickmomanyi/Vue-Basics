db7c6332f79cb985ac61f9d86a4970fc6cb694b7
derrickmomanyi <rickydee38@gmail.com>
2023-07-09 12:06:18 +0300

    Vue Bacics, routing, props, export, imports, games, structure

---
+++ vue-project/src/App.vue
@@ -0,0 +1,61 @@
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  data() {
+    return {
+     title: "My first Vue project",
+     header: "Modal Title",
+     text: "Let's try this",
+     header2: "Modal Title",
+     text2: "Let's try this",
+     showModal: false,
+     
+    };
+  },  
+  components: {
+      Modal
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo(){
+      this.showModal = !this.showModal
+    }
+  }
+};
+</script>
+
+
+<template> 
+ <h1 class="title">{{ title }}</h1>
+ <div v-if="showModal">
+  <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+ </div>
+ <div v-if="showModal">
+  <Modal :header="header" :text="text" @close="toggleModalTwo"/>
+ </div>
+ <h2>Welcome.....</h2>
+ <button @click="toggleModal" class="btn">Open Modal</button> 
+ <button @click="toggleModalTwo" class="btn">Open Modal 2</button> 
+ 
+
+</template>
+
+<style scoped>
+.title{
+  
+  text-align: center;
+  padding-top: 0px;
+  border-bottom: 1px solid #ddd;
+  /* display: inline-block; */
+  
+}
+
+h2{
+  text-align: center;
+}
+ .btn {
+  margin-left: 47.5%;
+}
+</style>
---
+++ vue-project/src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal">
+            <h1>{{header}}</h1>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+   
+</template>
+
+<script>
+export default {
+    props: ['header', 'text', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close')
+        }
+    }
+    }
+</script>
+
+<style scoped>
+.modal{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgb(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+h1, p{
+    text-align: center;
+    color: rgb(4, 149, 149);
+}
+</style>
